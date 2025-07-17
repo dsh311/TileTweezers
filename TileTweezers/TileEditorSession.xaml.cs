a1200e8a4f412c7ba7cc43df8d15fa4b37caaf9d
David S. Shelley <95400317+dsh311@users.noreply.github.com>
2025-07-17 16:18:42 -0700

    Add files via upload

--- TileTweezers/TileEditorSession.xaml.cs
+++ TileTweezers/TileEditorSession.xaml.cs
@@ -1,21 +1,5 @@
-/*
- * Copyright (C) 2025 David S. Shelley <davidsmithshelley@gmail.com>
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License 
- * along with this program. If not, see <https://www.gnu.org/licenses/>.
- */
-
-using _TileTweezers.Controls.TileEditorControl.TileEditorUtils;
+﻿using _TileTweezers.Controls.TileEditorControl.TileEditorUtils;
+using System;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
@@ -40,16 +24,30 @@ namespace _TileTweezers
             {
                 if (!_tileEditorControl_SourceInitialized)
                 {
+                    tileEditorControl_Source.OutputImage = tileEditorControl_Source.TileSetImage;
+
                     tileEditorControl_Destination.SourceTileControl = tileEditorControl_Source;
 
                     tileEditorControl_Destination.InputImage = tileEditorControl_Source.TileSetImage;
                     tileEditorControl_Destination.InputImagePreview = tileEditorControl_Source.TileSetImagePreview;
 
+                    // Hide the stamp tool on the left side
                     tileEditorControl_Source.stampBtn.Visibility = Visibility.Collapsed;
+
+                    tileEditorControl_Source.loadTilemap_Btn.Visibility = Visibility.Collapsed;
+                    tileEditorControl_Source.saveTilemapImgBtn.Visibility = Visibility.Collapsed;
+
+                    tileEditorControl_Source.tilesetFileNameTxtBox.Visibility = Visibility.Collapsed;
 
                     //Set this, or zooming in will be smoothed out and you wont see pixels as easily
                     RenderOptions.SetBitmapScalingMode(tileEditorControl_Source.TileSetImage, BitmapScalingMode.NearestNeighbor);
                     RenderOptions.SetBitmapScalingMode(tileEditorControl_Source.TileSetImagePreview, BitmapScalingMode.NearestNeighbor);
+
+
+                    // Choose the Stamp Selection tool
+                    tileEditorControl_Source.StampGridSelect_Click(tileEditorControl_Destination.stampSelectBtn, new RoutedEventArgs(Button.ClickEvent));
+
+
                 }
                 _tileEditorControl_SourceInitialized = true;
             };
@@ -60,13 +58,29 @@ namespace _TileTweezers
                 {
                     tileEditorControl_Source.DestinationTileControl = tileEditorControl_Destination;
 
+                    // Hide the selection stamp tool on the right side
+                    tileEditorControl_Destination.stampSelectBtn.Visibility = Visibility.Collapsed;
+
                     // Hide the color tools
                     tileEditorControl_Destination.ColorToolsGrid.Visibility = Visibility.Hidden;
                     tileEditorControl_Destination.bottomToolsBar.Visibility = Visibility.Hidden;
 
+                    // Change icon of open image file button
+                    tileEditorControl_Destination.tilesetOrmapImg.Source = new BitmapImage(
+                        new Uri("pack://application:,,,/Controls/TileEditorControl/TileEditorImages/tilemap.png"));
+                    
+                    tileEditorControl_Destination.loadTileset_Btn.Visibility = Visibility.Collapsed;
+                    tileEditorControl_Destination.saveTilesetImgBtn.Visibility = Visibility.Collapsed;
+
+                    //tileEditorControl_Destination.loadImg_Btn.Visibility = Visibility.Hidden;
+                    //tileEditorControl_Destination.loadImg_Btn.Image = ;
+                    //tileEditorControl_Destination.saveTilesetImgBtn.Visibility = Visibility.Hidden;
+
                     // Hide grid slice
                     tileEditorControl_Destination.GridSlicePanel.Visibility = Visibility.Hidden;
                     tileEditorControl_Destination.GridSlicePanel.IsEnabled = false;
+
+                    tileEditorControl_Destination.tilesetFileNameTxtBox.Visibility = Visibility.Collapsed;
 
                     //Set this, or zooming in will be smoothed out and you wont see pixels as easily
                     RenderOptions.SetBitmapScalingMode(tileEditorControl_Destination.TileSetImage, BitmapScalingMode.NearestNeighbor);
@@ -77,6 +91,16 @@ namespace _TileTweezers
                 }
                 _tileEditorControl_DestinationInitialized = true;
             };
+
+        }
+
+        private void About_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void OpenTilemap_Click(object sender, RoutedEventArgs e)
+        {
 
         }
     }
